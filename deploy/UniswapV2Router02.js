bc8cff059c7fa5e3c73bfe6149358eaa0fdc07af
Champagneswap <jq@zencoin.is>
2022-01-06 21:20:54 +0100

    Update UniswapV2Router02.js

--- deploy/UniswapV2Router02.js
+++ deploy/UniswapV2Router02.js
@@ -10,7 +10,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     let wethAddress;
 
     if (chainId === "31337") {
-      wethAddress = (await deployments.get("WETH9test")).address;
+      wethAddress = (await deployments.get("WETH9Mock")).address;
     } else if (chainId in WNATIVE_ADDRESS) {
       wethAddress = WNATIVE_ADDRESS[chainId];
     } else {
